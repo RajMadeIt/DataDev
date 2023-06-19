9ff859e77468944911a45bb6b51aa6057fc0a3a5
Raj-Lalwani <rajlalwani95o@gmail.com>
2023-06-20 00:25:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Analytics/>
       <Newsletter/>
       <Cards/>
+      <Footer/>
     </div>
   );
 }
